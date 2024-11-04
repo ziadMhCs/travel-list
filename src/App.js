523d2d8bfc3dd4cd694b7abee1b9db58614104e4
ziad <ziadoLearningGIT@mindyourownbusniss.com>
2024-11-04 05:16:20 -0800

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
+function Logo() {
+  return <h1>🌴Far Away👜</h1>;
+}
+
+function Form() {
+  function handelAdding(e){
+    e.preventDefault();
+const  {...target.input.value...c} = e;
+    console.log(target.input.value)    ;
+  }
+  return (
+    <form className="add-form" onSubmit={handelAdding}>
+      <h3> what do you need for your 😍 trip ? </h3>
+      <select>
+        <option value={1}> 1</option>
+        <option value={2}> 2</option>
+        <option value={3}> 3</option>
+      </select>
+      <input type="text" placeholder="item.."/>
+      <button >add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul className="">
+          {initialItems.map((i) => (
+            <Item key={i.id} itemobj={i} />
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+function Stats() {
+  return <footer className="stats">statisicts</footer>;
+}
+function Item({ itemobj }) {
+  return (
+    <li >
+      <span  style={ itemobj.packed ?{textDecoration:"line-through"}:{}}>
+        {itemobj.quantity}
+        {itemobj.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
 export default App;
