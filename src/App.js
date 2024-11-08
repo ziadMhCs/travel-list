a52f1a62148f081b34c07eecef53838b22c8341a
ziad <ziad.mh22cs@gmail.com>
2024-11-09 00:19:30 +0200

    1-add functionality of removing an item 2-add functionality of packing/unpacking an item.

--- src/App.js
+++ src/App.js
@@ -5,16 +5,33 @@ const initialquantity = [
   { id: 2, description: "Socks", quantity: 12, packed: true },
 ];
 function App() {
- const [items,setItems] = useState([]);
+ const [items,setItems] = useState(initialquantity);
  function handelAddItems(i){
    setItems(items=>[...items,i]);
  }    
+ function handelDeleteItem(id){
+  // console.log(e);
+  setItems(
+    items=>   items.filter(
+      (someItem)=>id!==someItem.id
+     ) 
+  )
+ }
+ function handelToggleItem(id){
+  setItems(
+    items=> 
+      items.map((i)=>i.id===id? {...i,packed:!i.packed}:i)
+    
+  )
+ }
   return (
 
     <div className="app">
       <Logo />
       <Form  onAddItem={handelAddItems}/>
-      <PackingList  itemsProp={items}/>
+      <PackingList  itemsProp={items} 
+        onDeleteItem={handelDeleteItem}
+        onToggleItems={handelToggleItem}/>
       <Stats />
     </div>
   );
@@ -39,7 +56,7 @@ function Form({onAddItem}) {
   }
   return (
     <form className="add-form" onSubmit={handelSubmit}>
-      <h3> what do you need for your 😍 trip ? </h3>
+      {/* <h3> what do you need for your 😍 trip ? </h3> */}
       <select value={quantity} onChange={e=> setQuantity(Number(e.target.value))}>
 
       {Array.from(
@@ -53,12 +70,14 @@ function Form({onAddItem}) {
   );
 }
 
-function PackingList({itemsProp}) {
+function PackingList({itemsProp,onDeleteItem,onToggleItems}) {
   return (
     <div className="list">
       <ul className="">
           {itemsProp.map((i) => (
-            <Item key={i.id}  itemobj={i} />
+            <Item key={i.id}  itemobj={i} 
+            onToggleItems={onToggleItems} 
+            onDeleteItem={onDeleteItem}/>
           ))}
       </ul>
     </div>
@@ -68,15 +87,21 @@ function PackingList({itemsProp}) {
 function Stats() {
   return <footer className="stats">statisicts</footer>;
 }
-function Item({ itemobj }) {
+function Item({ itemobj,onDeleteItem,onToggleItems }) {
   return (
     <li >
+
+        <input type="checkbox" 
+        // value={itemobj.packed}
+        checked={itemobj.packed}
+         onChange={()=>onToggleItems(itemobj.id)}
+         />
       <span  style={ itemobj.packed ?{textDecoration:"line-through"}:{}}>
-        {itemobj.quantity}
-        {": "}
-        {itemobj.description}
+        {itemobj.quantity} {itemobj.description}
       </span>
-      <button>❌</button>
+      <button onClick={()=>{
+        onDeleteItem(itemobj.id)}}
+      > ❌</button>
     </li>
   );
 }
